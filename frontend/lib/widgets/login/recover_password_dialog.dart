898d7794131f5134711ab93515e10c4a616d9a0a
Julian David <jdavcortes@poligran.edu.co>
2025-06-02 18:22:45 -0500

    Organizar carpetas y crear un widget reutilizable de ventanas emergentes para el futuro

---
+++ frontend/lib/widgets/login/recover_password_dialog.dart
@@ -0,0 +1,59 @@
+import 'package:flutter/material.dart';
+import 'package:frontend/widgets/dialogs/dialog_general.dart';
+
+void mostrarRecuperarContrasena(BuildContext context) {
+  final emailController = TextEditingController();
+
+  showDialog(
+    context: context,
+    builder: (context) => DialogoGeneral(
+      titulo: 'Recuperar contraseña',
+      contenido: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Text(
+            'Correo electrónico',
+            style: TextStyle(fontSize: 16, fontWeight: FontWeight.w500),
+          ),
+          SizedBox(height: 10),
+          TextField(
+            controller: emailController,
+            decoration: InputDecoration(
+              hintText: 'Ingrese su correo registrado',
+              filled: true,
+              fillColor: Colors.grey[50],
+              border: OutlineInputBorder(
+                borderRadius: BorderRadius.circular(6),
+              ),
+              contentPadding: EdgeInsets.symmetric(
+                horizontal: 12,
+                vertical: 12,
+              ),
+            ),
+          ),
+          SizedBox(height: 15),
+          Text(
+            'Se enviará un enlace para restablecer su contraseña.',
+            style: TextStyle(color: Colors.black87, fontSize: 14),
+          ),
+        ],
+      ),
+      textoBotonOk: 'Enviar',
+      onOk: () {
+        //Logica
+        String email = emailController.text;
+        if (email.isNotEmpty) {
+          print('Enviando email a: $email');
+          Navigator.pop(context);
+          ScaffoldMessenger.of(
+            context,
+          ).showSnackBar(SnackBar(content: Text('Email enviado')));
+        } else {
+          ScaffoldMessenger.of(
+            context,
+          ).showSnackBar(SnackBar(content: Text('Ingrese su correo')));
+        }
+      },
+    ),
+  );
+}
