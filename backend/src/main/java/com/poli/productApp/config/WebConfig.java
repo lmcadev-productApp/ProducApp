bdb87fc96e324e6cd7f2cc1153fdd15b2c4c8eda
Julian David <jdavcortes@poligran.edu.co>
2025-06-05 14:10:16 -0500

    configuración de CORS y ajuste de puerto en docker-compose
    - Se modificó WebConfig.java para permitir origen http://localhost:8082 desde el frontend.
    - Se corrigió el mapeo del puerto en docker-compose.yml; se cambió el puerto 80 al 81 del backend.

--- backend/src/main/java/com/poli/productApp/config/WebConfig.java
+++ backend/src/main/java/com/poli/productApp/config/WebConfig.java
@@ -14,7 +14,7 @@ public class WebConfig {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/**") // Permite CORS en todas las rutas
-                        .allowedOrigins("http://localhost:8080")
+                        .allowedOrigins("http://localhost:8082")
                         .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                         .allowedHeaders("*");
             }
