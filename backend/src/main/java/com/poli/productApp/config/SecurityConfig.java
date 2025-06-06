73a6eabdd92fadb1791ffedbefd097a6007c4bc3
Luis Miguel Castañeda <lmicastaneda@poligran.edu.co>
2025-06-05 19:58:04 -0500

    Se establece funciones de UsuarioController y se abre el CORS para que reciba peticiones de todos los puertos

--- backend/src/main/java/com/poli/productApp/config/SecurityConfig.java
+++ backend/src/main/java/com/poli/productApp/config/SecurityConfig.java
@@ -52,7 +52,8 @@ public class SecurityConfig {
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration config = new CorsConfiguration();
-        config.setAllowedOrigins(List.of("http://localhost:8082", "http://localhost:8080"));
+        //config.setAllowedOrigins(List.of("http://localhost:8082", "http://localhost:8080", "http://localhost:8081")); //solo para produccion
+        config.addAllowedOriginPattern("*"); // Solo en desarrollo
         config.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
         config.setAllowedHeaders(List.of("*"));
         config.setAllowCredentials(true); //
